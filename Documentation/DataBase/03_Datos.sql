cafe8b3afd2ec45e75a408b04597c6a2c66bae4d
Diego Gerena <dagerenaq@gmail.com>
2015-11-19 23:35:24 -0500

    Agregada columna en Usuario para ingresar los datos extra obtenidos del webService de Entidad Educativa, se modificó UsuarioEntity y UsuarioDAO para que se ajusten a los cambios.

--- Documentation/DataBase/03_Datos.sql
+++ Documentation/DataBase/03_Datos.sql
@@ -2,16 +2,16 @@
 -- INGRESAR DATOS TABLA USUARIO
 delete from USUARIO;
 
-insert into USUARIO values('1','1','CC','Nombre 1','Apellido 1','user1@unal.edu.co','user1','A');
-insert into USUARIO values('2','2','CC','Nombre 2','Apellido 2','user2@unal.edu.co','user2','P');
-insert into USUARIO values('3','3','CC','Nombre 3','Apellido 3','user3@unal.edu.co','user3','P');
-insert into USUARIO values('4','4','CC','Nombre 4','Apellido 4','user4@unal.edu.co','user4','P');
-insert into USUARIO values('5','5','CC','Nombre 5','Apellido 5','user5@unal.edu.co','user5','P');
-insert into USUARIO values('6','6','CC','Nombre 6','Apellido 6','user6@unal.edu.co','user6','E');
-insert into USUARIO values('7','7','CC','Nombre 7','Apellido 7','user7@unal.edu.co','user7','E');
-insert into USUARIO values('8','8','CC','Nombre 8','Apellido 8','user8@unal.edu.co','user8','E');
-insert into USUARIO values('9','9','CC','Nombre 9','Apellido 9','user9@unal.edu.co','user9','E');
-insert into USUARIO values('10','10','CC','Nombre 10','Apellido 10','user10@unal.edu.co','user10','E');
+insert into USUARIO values('1','1','CC','Nombre 1','Apellido 1','user1@unal.edu.co','user1','A',null);
+insert into USUARIO values('2','2','CC','Nombre 2','Apellido 2','user2@unal.edu.co','user2','P',null);
+insert into USUARIO values('3','3','CC','Nombre 3','Apellido 3','user3@unal.edu.co','user3','P',null);
+insert into USUARIO values('4','4','CC','Nombre 4','Apellido 4','user4@unal.edu.co','user4','P',null);
+insert into USUARIO values('5','5','CC','Nombre 5','Apellido 5','user5@unal.edu.co','user5','P',null);
+insert into USUARIO values('6','6','CC','Nombre 6','Apellido 6','user6@unal.edu.co','user6','E',null);
+insert into USUARIO values('7','7','CC','Nombre 7','Apellido 7','user7@unal.edu.co','user7','E',null);
+insert into USUARIO values('8','8','CC','Nombre 8','Apellido 8','user8@unal.edu.co','user8','E',null);
+insert into USUARIO values('9','9','CC','Nombre 9','Apellido 9','user9@unal.edu.co','user9','E',null);
+insert into USUARIO values('10','10','CC','Nombre 10','Apellido 10','user10@unal.edu.co','user10','E',null);
 
 -- INGRESAR TALLERES Y CURSOS LIBRES
 delete from TALLER;
--- Documentation/DataBase/README.md
+++ Documentation/DataBase/README.md
@@ -33,35 +33,3 @@ $ mysql -u bienestar -p
 use bienestar
 source 03_Datos.sql
 ```
-## Permitir conexiónes externas [Opcional]
-Si la base de datos se encuentra en un servidor distinto a la aplicación, se debe habilirar la conexión desde servidores externos. Para ello se debe abrir el archivo ***/etc/mysql/my.cnf*** , buscar la siguiente linea
-
-```sh
-bind-address = 127.0.0.1
-```
-
-y comentarla
-
-```sh
-#bind-address = 127.0.0.1
-```
-
-Luego reiniciar el servicio de MySQL
-
-```sh
-sudo service mysql restart
-```
-
-# Configuración jdbc en Glassfish
-La aplicaicón está configurada para que haga uso de un jdbc que se defina en Glassfish, está configuración está en la clase CrudDAO, en caso de que se desee cambiar el método para conectarse a la base de datos.
-
-Si se desea acceder al panel de administración de glassfish desde un servidor externo, se debe habilitar el acceso siguiendo los pasos en el siguiente link:
-
-[Spotlight on GlassFish 4.1: #11 Enable Remote Admin ](https://blogs.oracle.com/theaquarium/entry/spotlight_on_glassfish_4_18)
-
-Crear en Glassfish un jdbc llamado '*jdbc/bienestar*' siguiendo las instrucciones en el siguiente link:
-
-[How to setup a JDBC connection in Glassfish](https://computingat40s.wordpress.com/how-to-setup-a-jdbc-connection-in-glassfish/)
-
-
-
